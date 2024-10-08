0ed35ecf1bf9c7ba307b807546c0baa8944296bf
Sang Virgo <tansang06092004@gmail.com>
2024-10-08 23:48:34 +0700

    8-10-2024

---
+++ demo-dev/src/test/java/com/sang/thefirstapp/ThefirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang.thefirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThefirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
