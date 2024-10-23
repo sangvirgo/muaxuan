4860849982e5729d067b10b4c59e66e979f40caa
Sang Virgo <tansang06092004@gmail.com>
2024-10-23 22:57:30 +0700

    23-10-2024

---
+++ TheSecondApplication/src/test/java/com/sang/thesecondapplication/TheSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang.thesecondapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
