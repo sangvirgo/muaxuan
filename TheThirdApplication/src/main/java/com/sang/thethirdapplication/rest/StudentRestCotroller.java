62df2d9f0a13dd979d46ea7c22429129ef0e94b2
Sang Virgo <tansang06092004@gmail.com>
2024-11-13 23:20:20 +0700

    13/11/2024

--- TheThirdApplication/src/main/java/com/sang/thethirdapplication/rest/StudentRestCotroller.java
+++ TheThirdApplication/src/main/java/com/sang/thethirdapplication/rest/StudentRestCotroller.java
@@ -2,7 +2,9 @@ package com.sang.thethirdapplication.rest;
 
 
 import com.sang.thethirdapplication.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +15,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestCotroller {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Sang", "Nguyen"));
-        students.add(new Student("John", "Doe"));
-        students.add(new Student("Jane", "Doe"));
-        return students;
+    private List<Student> listStudent;
+
+//    init data
+    @PostConstruct
+    public void loadData() {
+        listStudent = new ArrayList<>();
+        listStudent.add(new Student("Sang", "Nguyen"));
+        listStudent.add(new Student("John", "Doe"));
+        listStudent.add(new Student("Jane", "Doe"));
     }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        return listStudent;
+    }
+
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return listStudent.get(studentId);
+    }
 
 }
