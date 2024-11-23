e66a3e94524e834423f6f1a850989440c2bb8944
Sang Virgo <tansang06092004@gmail.com>
2024-11-23 23:56:21 +0700

    23/11/2024

--- MiniProjectChapter3/src/main/java/com/sang/dao/EmployeeRepository.java
+++ MiniProjectChapter3/src/main/java/com/sang/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.sang.dao;
 
 import com.sang.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
