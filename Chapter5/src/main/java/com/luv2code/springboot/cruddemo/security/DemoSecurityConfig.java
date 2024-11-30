0e31f7cb12e7276fc1d33bced155d91de6af00d2
Sang Virgo <tansang06092004@gmail.com>
2024-11-30 23:57:32 +0700

    30/11/2024

--- Chapter5/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ Chapter5/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -9,32 +9,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-
+// add sup for jdbc authentication
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+        // Tùy chỉnh truy vấn nếu cần
+        userDetailsManager.setUsersByUsernameQuery(
+                "SELECT username, password, enable FROM users WHERE username = ?");
+        userDetailsManager.setAuthoritiesByUsernameQuery(
+                "SELECT username, authority FROM authorities WHERE username = ?");
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "ADMIN", "MANAGER")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return userDetailsManager;
     }
 
 
@@ -59,3 +53,29 @@ public class DemoSecurityConfig {
         return http.build();
     }
 }
+
+
+
+
+//@Bean
+//public InMemoryUserDetailsManager userDetailsManager() {
+//    UserDetails john = User.builder()
+//            .username("john")
+//            .password("{noop}test123")
+//            .roles("EMPLOYEE")
+//            .build();
+//
+//    UserDetails mary = User.builder()
+//            .username("mary")
+//            .password("{noop}test123")
+//            .roles("EMPLOYEE", "MANAGER")
+//            .build();
+//
+//    UserDetails susan = User.builder()
+//            .username("susan")
+//            .password("{noop}test123")
+//            .roles("EMPLOYEE", "ADMIN", "MANAGER")
+//            .build();
+//
+//    return new InMemoryUserDetailsManager(john, mary, susan);
+//}
--- sql/implement.sql
+++ sql/implement.sql
@@ -75,43 +75,49 @@
 
 USE `employee_directory`;
 
-DROP TABLE IF EXISTS `authorities`;
-DROP TABLE IF EXISTS `users`;
+-- DROP TABLE IF EXISTS `authorities`;
+-- DROP TABLE IF EXISTS `users`;
 
 
 
-CREATE TABLE `users` (
-	`username` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(50) NOT NULL,
-    `enable` TINYINT NOT NULL,
-    PRIMARY KEY (`username`)
-) ENGINE = InnoDB DEFAULT CHARSET=latin1;
+-- CREATE TABLE `users` (
+-- 	`username` VARCHAR(50) NOT NULL,
+--     `password` VARCHAR(50) NOT NULL,
+--     `enable` TINYINT NOT NULL,
+--     PRIMARY KEY (`username`)
+-- ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `users` 
-VALUES 
-('john', '{noop}test123', 1),
-('mary', '{noop}test123', 1),
-('susan', '{noop}test123', 1);
-
-
-
-CREATE TABLE `authorities` (
-	`username` VARCHAR(50) NOT NULL,
-    `authority` VARCHAR(50) NOT NULL,
-    UNIQUE KEY `authorities_idx_1` (`username`, `authority`),
-    CONSTRAINT `authorities_idx_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE = InnoDB DEFAULT CHARSET=latin1;
+-- INSERT INTO `users` 
+-- VALUES 
+-- ('john', '{noop}test123', 1),
+-- ('mary', '{noop}test123', 1),
+-- ('susan', '{noop}test123', 1);
 
 
 
+-- CREATE TABLE `authorities` (
+-- 	`username` VARCHAR(50) NOT NULL,
+--     `authority` VARCHAR(50) NOT NULL,
+--     UNIQUE KEY `authorities_idx_1` (`username`, `authority`),
+--     CONSTRAINT `authorities_idx_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+-- ) ENGINE = InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `authorities` 
-VALUES 
-('john','ROLE_EMPLOYEE'),
-('mary','ROLE_EMPLOYEE'),
-('mary','ROLE_MANAGER'),
-('susan','ROLE_EMPLOYEE'),
-('susan','ROLE_MANAGER'),
-('susan','ROLE_ADMIN');
 
+
+
+-- INSERT INTO `authorities` 
+-- VALUES 
+-- ('john','ROLE_EMPLOYEE'),
+-- ('mary','ROLE_EMPLOYEE'),
+-- ('mary','ROLE_MANAGER'),
+-- ('susan','ROLE_EMPLOYEE'),
+-- ('susan','ROLE_MANAGER'),
+-- ('susan','ROLE_ADMIN');
+
+--  SELECT * FROM authorities;
+-- SELECT * FROM users;-- 
+
+
+UPDATE `employee_directory`.`users` SET `password` = '{noop}test123' WHERE `username` = 'susan';
+SELECT * FROM users;
